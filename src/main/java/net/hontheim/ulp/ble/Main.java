b63c38b1d8e22ffe082ba456e62f2d28a8acbf6c
Henning Hontheim <henning.hontheim@unibw.de>
2020-06-10 19:23:59 +0200

    ?
    Signed-off-by: Henning Hontheim <henning.hontheim@unibw.de>

--- src/main/java/net/hontheim/ulp/ble/Main.java
+++ src/main/java/net/hontheim/ulp/ble/Main.java
@@ -1,7 +1,23 @@
 package net.hontheim.ulp.ble;
 
+import net.hontheim.ulp.ble.example.ExampleMain;
+import org.freedesktop.dbus.exceptions.DBusException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, DBusException {
         System.out.println("Hello!");
+        ExampleMain example = new ExampleMain();
+        System.out.println("");
+//		Thread t = new Thread(example);
+//		t.start();
+//		Thread.sleep(15000);
+        example.notifyBle("woooooo");
+//		Thread.sleep(15000);
+//		t.notify();
+
+//		Thread.sleep(5000);
+//		System.out.println("stopping application");
+        example.getApp().stop();
+        System.out.println("Application stopped");
     }
 }
